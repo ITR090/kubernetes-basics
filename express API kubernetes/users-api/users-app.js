b34a4904e36b718cd7f5a92773faab8d11e94881
ITR090 <raghad.4309@gmail.com>
2025-06-14 19:32:23 +0300

    adding to express API project

--- express API kubernetes/users-api/users-app.js
+++ express API kubernetes/users-api/users-app.js
@@ -11,19 +11,13 @@ app.post('/signup', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (
-    !password ||
-    password.trim().length === 0 ||
-    !email ||
-    email.trim().length === 0
-  ) {
-    return res
-      .status(422)
-      .json({ message: 'An email and password needs to be specified!' });
+  if (!password || password.trim().length === 0 || !email || email.trim().length === 0) {
+    return res.status(422).json({ message: 'An email and password needs to be specified!' });
   }
 
   try {
-    const hashedPW = await axios.get('http://auth/hashed-password/' + password);
+    const hashedPW = await axios.get(`http://auth-service:80/hashed-password/` + password);
+    // const hashedPW = 'dummy text';
     // since it's a dummy service, we don't really care for the hashed-pw either
     console.log(hashedPW, email);
     res.status(201).json({ message: 'User created!' });
@@ -54,8 +48,9 @@ app.post('/login', async (req, res) => {
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + '_hash';
   const response = await axios.get(
-    'http://auth/token/' + hashedPassword + '/' + password
+    `http://auth-service:80/token/` + hashedPassword + '/' + password
   );
+  // const response = { status: 200, data: { token: 'abc' } };
   if (response.status === 200) {
     return res.status(200).json({ token: response.data.token });
   }
